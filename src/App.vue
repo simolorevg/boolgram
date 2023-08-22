ce25ce4ebec69c991f4efade13b56176684650cc
simolorevg <simoneviaggi@libero.it>
2023-08-22 16:03:16 +0200

    Made the structure

--- src/App.vue
+++ src/App.vue
@@ -2,33 +2,99 @@
 
 </script>
 
-<template></template>
+<template>
+  <div class="container">
+    <div class="left-container">
+      <div class="stories">
+        Stories
+      </div>
+      <div class="posts">
+        <div class="post">
+          <div class="post-img">
+            Img here
+          </div>
+          <div class="post-content">
+            Content Here
+          </div>
+        </div>
+      </div>
+    </div>
+    <!---->
+    <div class="right-container">
+      <div class="profile">
+        Your profile here
+      </div>
+      <div class="direct">
+        All messages
+      </div>
+    </div>
+  </div>
+</template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<style lang="scss" scoped>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+.container {
+  width: 100%;
+  height: 100vh;
+  display: flex;
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+  .left-container {
+    width: 60%;
+    height: 100%;
+    border: 1px solid red;
+    position: relative;
+
+    .stories {
+      width: 100%;
+      height: 20%;
+      background-color: blue;
+    }
+
+    .posts {
+      width: 100%;
+      height: 80%;
+      background-color: aquamarine;
+      padding: 10px;
+
+      .post {
+        width: 60%;
+        height: 400px;
+        margin: 0 auto;
+        border: 2px solid black;
+        display: flex;
+        flex-direction: column;
+
+        .post-img {
+          width: 100%;
+          height: 60%;
+          background-color: crimson;
+        }
+      }
+    }
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+  .right-container {
+    flex-grow: 1;
+    height: 100%;
+    border: 2px solid black;
+    position: relative;
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+    .profile {
+      width: 100%;
+      height: 20%;
+      background-color: purple;
+    }
+
+    .direct {
+      width: 100%;
+      height: 80%;
+      background-color: pink;
+    }
   }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
-
-
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import { createApp } from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+app.use(VueAxios, axios)
