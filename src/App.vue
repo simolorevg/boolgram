700361d51a8e29f49a34853dfacd5ee11d333749
simolorevg <simoneviaggi@libero.it>
2023-08-23 10:26:31 +0200

    made some structure of stories and profile

--- src/App.vue
+++ src/App.vue
@@ -5,53 +5,62 @@
 <template class="position">
   <div class="container">
     <div class="search-bar">
-      <img src="src\imgs\logo.png" alt="logomilanfanclub">
-      <img src="src\imgs\Milan Club.png" alt="title">
+      <img src="src\imgs\logo.png" alt="logo">
     </div>
     <div class="top-container">
       <div class="stories">
-        Stories
+        <div class="stories-container">
+          <div class="story">
+            <div class="story-profile">
+
+            </div>
+            <div class="story-user">
+              username
+            </div>
+          </div>
+        </div>
       </div>
       <div class="profile">
-        <div class="logo-user"></div>
+        <div class="logo-user">
+
+        </div>
         <div class="username">
-          Alessio Rossi
+          Mario Rossi
         </div>
       </div>
     </div>
     <!---->
     <div class="bottom-container">
+      <!--POST CONTAINER-->
       <div class="posts">
+
+        <!--SINGLE POST-->
         <div class="post">
+          <div class="post-user">
+            <div>
+              <img src="" alt="Profile"> <span>username</span>
+            </div>
+            options
+          </div>
           <div class="post-img">
             <img
               src="https://tmssl.akamaized.net/images/foto/galerie/olivier-giroud-ac-mailand-2022-1644135835-79775.jpg?lm=1644135848"
               alt="">
           </div>
           <div class="post-content">
-            Olivier Giroud, segna ancora una volta. Contro il Bologna si è girato Giroud!!!!!
+
           </div>
         </div>
-        <div class="post">
-          <div class="post-img">
-            Img here
-          </div>
-          <div class="post-content">
-            Content Here
-          </div>
-        </div>
-        <div class="post">
-          <div class="post-img">
-            Img here
-          </div>
-          <div class="post-content">
-            Content Here
-          </div>
-        </div>
+        <!--/SINGLE POST-->
+
       </div>
+      <!--/POST CONTAINER-->
+
+      <!--DIRECT CONTAINER-->
       <div class="direct">
         All messages
       </div>
+      <!--/DIRECT CONTAINER-->
     </div>
   </div>
 </template>
@@ -92,12 +101,56 @@
     width: 100%;
     display: flex;
     margin-top: 100px;
-    border: 1px solid gold;
 
     .stories {
       width: 60%;
       height: 90px;
-      border: 2px solid black;
+      display: flex;
+      align-items: center;
+
+      .stories-container {
+        border: 1px solid grey;
+        border-radius: 5px;
+        width: 70%;
+        height: 100%;
+        margin: 0 auto;
+        display: flex;
+        align-items: center;
+        padding: 5px;
+
+        .story {
+          width: 80px;
+          height: 80px;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+
+          .story-profile {
+            width: 70%;
+            height: 70%;
+            margin: 0 auto;
+            background-color: red;
+            border-radius: 50%;
+          }
+        }
+      }
+    }
+
+    .profile {
+      display: flex;
+      align-items: center;
+      flex-grow: 1;
+      padding: 5px;
+      border: 1px solid black;
+
+      .logo-user {
+        width: 80px;
+        height: 80px;
+        background-color: red;
+        border-radius: 50%;
+        margin-right: 10px;
+      }
     }
   }
 
@@ -107,32 +160,46 @@
 
     .posts {
       width: 60%;
-      border: 2px solid yellow;
       padding: 10px;
 
       .post {
-        width: 60%;
+        max-width: 70%;
         margin: 0 auto;
-        height: 400px;
+        height: 500px;
         display: flex;
         flex-direction: column;
+        border: 1px solid grey;
+        border-radius: 5px;
+
+        .post-user {
+          width: 100%;
+          height: 50px;
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+        }
 
         .post-img {
-          width: 100%;
-          height: 60%;
-          border: 1px solid black;
+          max-width: 100%;
+          max-height: 60%;
 
           img {
-            width: 464px;
-            height: 238px;
+            width: 544px;
+            height: 300px;
           }
         }
 
         .post-content {
           flex-grow: 1;
-          background-color: gold;
         }
       }
+    }
+
+    .direct {
+      padding: 5px;
+      border: 1px solid grey;
+      border-radius: 5px;
+      flex-grow: 1;
     }
   }
 }
